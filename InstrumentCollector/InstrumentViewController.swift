9f5127cb549d2dbd8545f97ab172fdfeb34b8293
Mitch Guzman <mguzman9294@gmail.com>
2017-03-11 09:40:32 -0700

    Began building story board, everything connected to code. No code added for what to do when actions are tapped yet.

---
+++ InstrumentCollector/InstrumentViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InstrumentViewController.swift
+//  InstrumentCollector
+//
+//  Created by Mitch Guzman on 3/11/17.
+//  Copyright © 2017 Mitch Guzman. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var instrumentImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
